f72175e972e02f4ce59555b8cb784ef6a0311277
sujitv19196 <sujit5817@gmail.com>
2023-04-27 20:32:09 -0500

    bruh

--- test_client/random-bidding-test.py
+++ test_client/random-bidding-test.py
@@ -27,37 +27,32 @@ def generate_random_point_in_CU():
     return (new_lat, new_long) 
 
 def run_executable(executable_path, num_ittr):
-    """
-    Runs an executable file n times and logs the output.
-
-    Parameters:
-    executable_path (str): The path to the executable file.
-    n (int): The number of times to run the executable file.
-
-    Returns:
-    None.
-    """
-
     times = [0]
     for i in range(num_ittr):
         nodeType = random.randint(0, 1)
         startLat, startLng = generate_random_point_in_CU()
         destLat, destLng = generate_random_point_in_CU()
-    
+
         result = None
+        output = None
         try: 
-            result = subprocess.run([executable_path, str(nodeType), "172.22.150.238", str(startLat), str(startLng), str(destLat), str(destLng)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout=90)
-        except subprocess.TimeoutExpired:
-            # If the program times out, send SIGKILL to the process
-            result = subprocess.CompletedProcess(args=[], returncode=-9, stdout=b"", stderr=b"SIGKILL sent to process\n")
-        
-        s = str(result.stdout)
+            result = subprocess.run([executable_path, str(nodeType), "172.22.150.238", str(startLat), str(startLng), str(destLat), str(destLng)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout=10)
+            output = result.stdout
+        except subprocess.TimeoutExpired as e:
+            # If the program times out, save the output before sending SIGINT to the process
+            output = e.stdout
+            # result = subprocess.CompletedProcess(args=[], returncode=-2, stdout=output, stderr=b"SIGINT sent to process\n")
+            # subprocess.run(['kill', '-2', str(result.pid)])  # Send SIGINT to the process
+
+        s = str(output)
         output_lines = s.split('\n')
         print(output_lines)
         for line in output_lines:
             match = re.search(r'Time: (\d+\.\d+)ms', line)
             if match:
                 times.append(float(match.group(1)))
+
     average = sum(times) / len(times)
     print(average)
+
 run_executable("client/client", 2)
