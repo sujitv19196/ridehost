bd921e23e5d6d86e12793590c8d407778a0578d7
sujitv19196 <sujit5817@gmail.com>
2023-05-05 13:20:29 -0500

    add ports to bid

--- client/client.go
+++ client/client.go
@@ -242,7 +242,7 @@ func distanceBetweenCoords(lat1 float64, lng1 float64, lat2 float64, lng2 float6
 ////////////////////////////////////////////////////////////////////////////////
 
 func sendBidRPC(node types.Node, cost float64) types.BidResponse {
-	conn, err := net.DialTimeout("tcp", node.Ip, constants.BidTimeout)
+	conn, err := net.DialTimeout("tcp", node.Ip+":"+strconv.Itoa(constants.Ports["clientRPC"]), constants.BidTimeout)
 	if err != nil {
 		os.Stderr.WriteString(err.Error() + "\n")
 		return BidResponse{Response: false}
@@ -259,7 +259,7 @@ func sendBidRPC(node types.Node, cost float64) types.BidResponse {
 }
 
 func sendDriveInfo(node Node, driverInfo DriverInfo) RiderInfo {
-	conn, err := net.DialTimeout("tcp", node.Ip, constants.BidTimeout)
+	conn, err := net.DialTimeout("tcp", node.Ip+":"+strconv.Itoa(constants.Ports["clientRPC"]), constants.BidTimeout)
 	if err != nil {
 		os.Stderr.WriteString(err.Error() + "\n")
 		return RiderInfo{Response: false}
